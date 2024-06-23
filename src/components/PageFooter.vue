8399cbaf6c21941345b976b020f1ef78edaf5acc
Ludovit Scholtz <ludko2@gmail.com>
2024-06-23 17:15:48 +0200

    biatec identity fe init

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import Card from 'primevue/card'
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+const isMobile = ref(false)
+
+function handleResize() {
+  if (window.innerWidth <= 768) {
+    isMobile.value = true
+  } else {
+    isMobile.value = false
+  }
+}
+
+onMounted(() => {
+  if (window.innerWidth <= 768) {
+    isMobile.value = true
+  }
+  window.addEventListener('resize', handleResize)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', handleResize)
+})
+</script>
+<template>
+  <Card class="m-2 text-center">
+    <template #content>
+      Biatec Identity is part of the Biatec Group -
+      <a href="https://www.biatec.io">https://www.biatec.io</a>
+    </template>
+  </Card>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import Layout from '@/layouts/PublicLayout.vue'
+import Card from 'primevue/card'
+import Button from 'primevue/button'
+
+/* eslint-disable no-useless-escape */
+</script>
+<template>
+  <Layout :hideTopMenu="false">
+    <div>
+      <div class="m-2 md:m-6">
+        <Card class="col-12 md:col-offset-3 md:col-6 my-4">
+          <template #content>
+            <h1>Join the biatec identity waiting list</h1>
+            <p>First to join will have priority in usage of our services:</p>
+            <ul>
+              <li>up to 2x lower liqudity pool fees when trading at the Biatec DEX</li>
+              <li>biatec engagement level increased</li>
+              <li>
+                identity verification will be processed on FIFO bases, sooner you enter the waiting
+                list, sooner you will receive benefits
+              </li>
+            </ul>
+            <a href="https://forms.gle/XSXgjwLHuF5CL8Kd8">
+              <Button>Join waiting list</Button>
+            </a>
+          </template>
+        </Card>
+      </div>
+    </div>
+  </Layout>
+</template>
+<style>
+.svg-image {
+  height: 8em;
+}
+</style>
