7d73b2d27b330d061d9bd1883e9264761f465afb
Scholtz <ludko2@gmail.com>
2024-06-23 17:23:59 +0200

    cicd deploy

--- docker/compose.sh
+++ docker/compose.sh
@@ -2,19 +2,19 @@ if [ "$ver" == "" ]; then
 ver=1.0.0
 fi
 
-echo "docker build -t \"scholtz2/biatec-learn:$ver-main\" -f Dockerfile .."
-nice -n 20 docker build -t "scholtz2/biatec-learn:$ver-main" -f Dockerfile .. || error_code=$?
+echo "docker build -t \"scholtz2/biatec-identity:$ver-main\" -f Dockerfile .."
+nice -n 20 docker build -t "scholtz2/biatec-identity:$ver-main" -f Dockerfile .. || error_code=$?
 if [ "$error_code" != "" ]; then
 echo "$error_code";
     echo "failed to build";
 	exit 1;
 fi
 
-docker push "scholtz2/biatec-learn:$ver-main" || error_code=$?
+docker push "scholtz2/biatec-identity:$ver-main" || error_code=$?
 if [ "$error_code" != "" ]; then
 echo "$error_code";
     echo "failed to push";
 	exit 1;
 fi
 
-echo "Image: scholtz2/biatec-learn:$ver-main"
+echo "Image: scholtz2/biatec-identity:$ver-main"
--- k8s/mainnet/update-config.sh
+++ k8s/mainnet/update-config.sh
@@ -1,4 +1,5 @@
 kubectl apply -f deployment-main.yaml -n biatec
-kubectl delete configmap biatec-learn-main-conf -n biatec
-kubectl create configmap biatec-learn-main-conf --from-file=conf -n biatec
-kubectl rollout restart deployment/biatec-learn-main-deployment -n biatec
+kubectl delete configmap biatec-identity-main-conf -n biatec
+kubectl create configmap biatec-identity-main-conf --from-file=conf -n biatec
+kubectl rollout restart deployment/biatec-identity-main-deployment -n biatec
+kubectl rollout status deployment/biatec-identity-main-deployment -n biatec
